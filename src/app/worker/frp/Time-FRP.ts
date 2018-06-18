7fb1cf21fb23eaa688957ca0f45e6f2848196045
David Komer <david.komer@gmail.com>
2018-06-18 09:03:45 -0400

    comments

--- src/app/worker/frp/Time-FRP.ts
+++ src/app/worker/frp/Time-FRP.ts
@@ -2,9 +2,17 @@ import {sWorldUpdate} from "./World-Sinks-FRP";
 import {S} from "utils/Utils";
 import {Cell, TimerSystem, TimerSystemImpl, StreamSink, Operational, Stream} from "sodiumjs";
 
+export interface Alarm {
+    ts:number;
+    cancel: () => void;
+}
+
+//tick on worker side is driven by requestAnimationFrame on the main side
 export const sTick = 
     sWorldUpdate.map(({frameTs}) => frameTs);
 
+//simple way to filter out functions that shouldn't fire until first tick
+//currently just for local use in mapStreamTick but could have more purposes
 const cMaybeTick = sTick.map(S.Just).hold(S.Nothing);
 
 //Allows mixing in a timestamp to a stream
@@ -19,18 +27,10 @@ export const mapStreamTick = <T>(sSource:Stream<T>):Stream<T & {ts: number}> =>
     .map (S.maybe (undefined) (S.I));
 
 
-//Sends an event after the Cell's value (in ms) both on init and subsequent changes
-//If the value is < 0 it won't be sent
-//
-export interface Alarm {
-    ts:number;
-    cancel: () => void;
-}
 
-//Creates a stream which fires after the seconds in cTime
-//Including re-firing when cTime changes
-//Note that the actual time of firing is n, but it is filtered and mapped
-//According to the tick time, for the sake of consistency
+//Creates a stream which fires after the seconds in cTime (if time is <0, it's not fired)
+//Will re-fire when cTime changes
+//Note that the actual time of firing is n, but it is filtered and mapped by frame tick
 export const alarmUpdates = (cTime:Cell<number>) => {
     const sSink = new StreamSink<{cancel: () => void}>();
 
