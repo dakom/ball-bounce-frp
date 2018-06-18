f216e77ceae3c938697b9bba1996dff51a6727fc
David Komer <david.komer@gmail.com>
2018-06-18 11:53:14 -0400

    small refactor

--- src/app/worker/frp/Ball-FRP.ts
+++ src/app/worker/frp/Ball-FRP.ts
@@ -33,68 +33,7 @@ interface BallPhysicsBounce extends BallPhysics {
     v: number;
 }
 
-//Hardcoded physics values
-const GRAVITY = -9.81;
-const RESTITUTION = .8;
-
-//Hardcoded point of impact
-const BOTTOM = -0.903; 
-
-//Lenses
-const HEIGHT = "h";
-const VELOCITY = "v";
-const GET_POSITION = "getPosition"
-const NEXT_BOUNCE_TIME = "nextBounceTime";
-const START_TIME = "t0";
-
-//Helper to set a lens based on function which gets the object
-const setLens = <T,V>(lens:any) => (fn:(obj:T) => V) => (obj:T):T =>
-    L.set (lens) (fn(obj)) (obj);
-
-//Initial Physics - just a drop until impact
-//The starting input is a plain GltfNode
-//createInitialPhysics :: Int -> GltfNode -> BallPhysicsDrop
-const createInitialPhysics = (now:number) =>
-    S.pipe([
-        (node:GltfNode) => ({y: node.transform.trs.translation[1]}),
-
-        setLens (HEIGHT)
-                ((phys:BallPhysicsDrop) => phys.y - BOTTOM),
-
-        setLens (GET_POSITION)
-                ((phys:BallPhysicsDrop) => (t:number) => phys.y + Kinematic3(0) (GRAVITY) (t)),
-
-        setLens (NEXT_BOUNCE_TIME)
-                ((phys:BallPhysicsDrop) => Kinematic5 (0) (GRAVITY) (-phys.h)),
-
-        setLens (START_TIME)
-                (() => now),
-    ]) 
-
-//Bounce Physics - graphs like a parabola from start to end.
-//The starting input may be from a drop or bounce (e.g. anything with height)
-//It is assumed that the input is currently at BOTTOM 
-//onBounce :: Int -> BallPhysics -> BallPhysicsBounce
-const onBounce = (now:number) => 
-    S.pipe([
-        setLens (HEIGHT) 
-                ((phys:BallPhysics) => phys.h * RESTITUTION),
-
-        setLens (VELOCITY) 
-                ((phys:BallPhysics) => Kinematic4 (0) (-GRAVITY) (phys.h)),
-
-        setLens (GET_POSITION) 
-                ((phys:BallPhysicsBounce) => (t:number) => BOTTOM + Kinematic3 (phys.v) (GRAVITY) (t)),
-
-        setLens (NEXT_BOUNCE_TIME) 
-                ((phys:BallPhysicsBounce) => Kinematic7 (0) (-GRAVITY) (phys.v) * 2),
-
-        setLens (START_TIME)
-                (() => now),
-    ]); 
-
-
-//Export
+//Main logic
 export const createBallFrp = (node:GltfNode) => {
 
     const cBall:Cell<Maybe<BallPhysics>> = Transaction.run(() => {
@@ -157,3 +96,62 @@ export const createBallFrp = (node:GltfNode) => {
     return sBallUpdates;
 }
 
+//Hardcoded physics values
+const GRAVITY = -9.81;
+const RESTITUTION = .8;
+
+//Hardcoded point of impact
+const BOTTOM = -0.903; 
+
+//Lenses
+const HEIGHT = "h";
+const VELOCITY = "v";
+const GET_POSITION = "getPosition"
+const NEXT_BOUNCE_TIME = "nextBounceTime";
+const START_TIME = "t0";
+
+//Helper to set a lens based on function which gets the object
+const setLens = <T,V>(lens:any) => (fn:(obj:T) => V) => (obj:T):T =>
+    L.set (lens) (fn(obj)) (obj);
+
+//Initial Physics - just a drop until impact
+//The starting input is a plain GltfNode
+//createInitialPhysics :: Int -> GltfNode -> BallPhysicsDrop
+const createInitialPhysics = (now:number) =>
+    S.pipe([
+        (node:GltfNode) => ({y: node.transform.trs.translation[1]}),
+
+        setLens (HEIGHT)
+                ((phys:BallPhysicsDrop) => phys.y - BOTTOM),
+
+        setLens (GET_POSITION)
+                ((phys:BallPhysicsDrop) => (t:number) => phys.y + Kinematic3(0) (GRAVITY) (t)),
+
+        setLens (NEXT_BOUNCE_TIME)
+                ((phys:BallPhysicsDrop) => Kinematic5 (0) (GRAVITY) (-phys.h)),
+
+        setLens (START_TIME)
+                (() => now),
+    ]) 
+
+//Bounce Physics - graphs like a parabola from start to end.
+//The starting input may be from a drop or bounce (e.g. anything with height)
+//It is assumed that the input is currently at BOTTOM 
+//onBounce :: Int -> BallPhysics -> BallPhysicsBounce
+const onBounce = (now:number) => 
+    S.pipe([
+        setLens (HEIGHT) 
+                ((phys:BallPhysics) => phys.h * RESTITUTION),
+
+        setLens (VELOCITY) 
+                ((phys:BallPhysics) => Kinematic4 (0) (-GRAVITY) (phys.h)),
+
+        setLens (GET_POSITION) 
+                ((phys:BallPhysicsBounce) => (t:number) => BOTTOM + Kinematic3 (phys.v) (GRAVITY) (t)),
+
+        setLens (NEXT_BOUNCE_TIME) 
+                ((phys:BallPhysicsBounce) => Kinematic7 (0) (-GRAVITY) (phys.v) * 2),
+
+        setLens (START_TIME)
+                (() => now),
+    ]); 
